75e3705fdb7d64a4ed21186d5efde4b9e8f19154
cmarcosantander7 <marco.santander@trescloud.com>
2024-09-16 18:37:51 -0500

    Add lab4

--- test.py
+++
@@ -1,5 +0,0 @@
-from instituto.clase import estudiante_registrado_en_materia
-
-print(estudiante_registrado_en_materia("Daniel", "Matematica"))  # True
-print(estudiante_registrado_en_materia("Daniel", "Biologia"))    # False
-print(estudiante_registrado_en_materia("Marco", "Biologia"))    # False
